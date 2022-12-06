3520b175337a0f48d2c3db737c246914dc0a96b7
dlwnsrb0829 <ijg0829@naver.com>
2022-12-06 15:43:47 +0900

    toggle

--- app/src/main/java/com/example/softwareengineering/MainActivity.java
+++ app/src/main/java/com/example/softwareengineering/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox cb1;
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox cb4;
     private CheckBox cb5;
     private CheckBox cb6;
+    private Button record_activity_toggle;
+    private LinearLayout emotion_add_layout;
+    private boolean isToggle = true;
 
 
     @Override
@@ -26,9 +31,24 @@ public class MainActivity extends AppCompatActivity {
         cb5 = (CheckBox) findViewById(R.id.checkbox5);
         cb6 = (CheckBox) findViewById(R.id.checkbox6);
         CheckBox[] cb_array = {cb1, cb2, cb3, cb4, cb5, cb6};
+        record_activity_toggle = (Button) findViewById(R.id.record_activity_toggle);
+        emotion_add_layout = (LinearLayout) findViewById(R.id.emotion_add_layout);
+        record_activity_toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isToggle){
+                    record_activity_toggle.setText("▲");
+                    emotion_add_layout.setVisibility(View.VISIBLE);
+                    isToggle = false;
+                }else{
+                    record_activity_toggle.setText("▼");
+                    emotion_add_layout.setVisibility(View.GONE);
+                    isToggle = true;
+                }
+            }
+        });
+
         initCheckBox(cb_array);
-
-
     }
 
     private void initCheckBox(CheckBox[] cb_array){
