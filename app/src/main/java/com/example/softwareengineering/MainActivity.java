32e08b0b7c5e8e9b05d798f485e64ed37ab7267c
dlwnsrb0829 <ijg0829@naver.com>
2022-12-06 15:34:59 +0900

    checkbox click

--- app/src/main/java/com/example/softwareengineering/MainActivity.java
+++ app/src/main/java/com/example/softwareengineering/MainActivity.java
@@ -3,9 +3,8 @@ package com.example.softwareengineering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox cb1;
@@ -26,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         cb4 = (CheckBox) findViewById(R.id.checkbox4);
         cb5 = (CheckBox) findViewById(R.id.checkbox5);
         cb6 = (CheckBox) findViewById(R.id.checkbox6);
+        CheckBox[] cb_array = {cb1, cb2, cb3, cb4, cb5, cb6};
+        initCheckBox(cb_array);
 
+
+    }
+
+    private void initCheckBox(CheckBox[] cb_array){
+        for(int i = 0 ; i < 6 ; i++){
+            int num = i;
+            cb_array[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    for(int j = 0 ; j < 6 ; j++){
+                        cb_array[j].setChecked(false);
+                    }
+                    cb_array[num].setChecked(true);
+                }
+            });
+        }
     }
 }
